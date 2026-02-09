bf3cd59485bc548d0c16df879762314e2bd74be9
kevin <kevinagera59@gmail.com>
2026-02-09 11:20:36 +0530

    sql interface

--- app.py
+++ app.py
@@ -4,7 +4,22 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from Flask + Git!"
+    return "Home Page"
+@app.route("/about")
+def about():
+    return "About Page"
+@app.route("/contact")
+def contact():
+    return "contact page"
+@app.route("/user/<name>")
+def user(name):
+    return f"hello {name}!"
+@app.route("/product/<id>")
+def product():
+    return f"product id is {id}!"
+@app.route("/square/<int:num>")
+def square():
+    return f"square is{num*num}"
 
 if __name__ == "__main__":
     app.run(debug=True)
--- app1.py
+++ app1.py
@@ -1,8 +1,25 @@
 from flask import Flask,jsonify
+import mysql.connector
 app=Flask(__name__)
+def get_db_connection():
+    return mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="chickachickaslimshady",
+        database="flask_test"
+    )
 @app.route("/api/data")
-def get_data():
-    return jsonify({"name":"kevin","role":"backend developer","stack":"flask+mysql"})
+def get_users():
+    conn = get_db_connection()
+    cursor = conn.cursor(dictionary=True)
+
+    cursor.execute("SELECT * FROM users")
+    users = cursor.fetchall()
+
+    cursor.close()
+    conn.close()
+
+    return jsonify(users)
 if __name__=="__main__":
     app.run(debug=True)
 
