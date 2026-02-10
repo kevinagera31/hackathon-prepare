f319b522f9a9a591caeba116eb07572d6f6985af
Pavan Prabhu <pavanprabhu654321@gmail.com>
2026-02-10 11:47:13 +0530

    mjkjk

--- app.py
+++ app.py
@@ -15,11 +15,17 @@ def contact():
 def user(name):
     return f"hello {name}!"
 @app.route("/product/<id>")
-def product():
+def product(id):
     return f"product id is {id}!"
 @app.route("/square/<int:num>")
-def square():
+def square(num):
     return f"square is{num*num}"
+@app.route("/add/<int:a>/<int:b>")
+def add(a,b):
+    return f"add a and b = {a+b}"
+@app.route("/work/<name>")
+def work(name):
+    return f"{name} You think your cute , now stop smilling and laughing and do your work !!!!!"
 
 if __name__ == "__main__":
     app.run(debug=True)
